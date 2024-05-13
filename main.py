1ae7e6c8630ff5086b24cf5314b48f88094a5ff5
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-05-13 16:42:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-cmd = "sudo apt-get install wget coreutils"
-cmd1 = "wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh" 
+cmd = "sudo apt-get install coreutils"
+cmd1 = "chmod 755 install_ant-media-server.sh" 
 cmd2 = "sudo ./install_ant-media-server.sh"
 
 os.system(cmd + "&&" + cmd1 + "&&" + cmd2)
