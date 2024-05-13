11b7a8e34d849cc5a2eccc4c493a087f67e5ef5e
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-05-13 17:47:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 cmd = "sudo apt-get install coreutils"
 cmd1 = "chmod 755 install_ant-media-server.sh" 
 cmd2 = "sudo ./install_ant-media-server.sh"
-cmd3 = "sudo ./src/main/server/start.sh"
+cmd3 = "sudo service antmedia start"
 os.system(cmd + "&&" + cmd1 + "&&" + cmd2+ "&&" +cmd3)
